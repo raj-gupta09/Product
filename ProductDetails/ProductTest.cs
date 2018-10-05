bbbfa9e573fa41dd271084366fa66387adb048b7
raj-gupta09 <rajk.gupta0910@gmail.com>
2018-10-05 09:31:05 +0530

    Update ProductTest.cs

--- ProductDetails/ProductTest.cs
+++ ProductDetails/ProductTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,51 +8,60 @@ namespace ProductDetails
 {
     public class ProductTest
     {
+        private static void GetProductDetails(ProductMock P)
+        {
+            Console.Write("Enter Product ID :");
+            P.ProductID = Convert.ToInt32(Console.ReadLine());
+            if (P.ProductID <= 0)
+                throw new DataEntryException("Product ID must be greater than Zero");
+
+            Console.Write("Enter Product Name :");
+            P.ProductName = Console.ReadLine();
+            if (P.ProductName == string.Empty)
+                throw new DataEntryException("Product Name cannot be left blank");
+
+            Console.Write("Enter Price :");
+            P.ProductPrice = Convert.ToDouble(Console.ReadLine());
+            if (P.ProductPrice <= 0)
+                throw new DataEntryException("Price of product must be greater than Zero");
+        }
+
+        private static void DisplayDetails(ProductMock P)
+        {
+            Console.WriteLine("Product Details :");
+            Console.WriteLine("Product ID :" + P.ProductID);
+            Console.WriteLine("Product Name :" + P.ProductName);
+            Console.WriteLine("Product Price :" + P.ProductPrice);
+
+        }
+
         static void Main(string[] args)
         {
 
             ProductMock P = new ProductMock();
 
-            do
+            try
             {
-                try
-                {
-                    Console.Write("Enter Product ID :");
-                    P.ProductID = Convert.ToInt32(Console.ReadLine());
-                    if (P.ProductID < 0)
-                        throw new DataEntryException("Product ID must be greater than Zero");
-
-                    Console.Write("Enter Product Name :");
-                    P.ProductName = Console.ReadLine();
-                    if (P.ProductName == string.Empty)
-                        throw new DataEntryException("Product Name cannot be left blank");
-
-                    Console.Write("Enter Price :");
-                    P.ProductPrice = Convert.ToDouble(Console.ReadLine());
-                    if (P.ProductPrice < 0)
-                        throw new DataEntryException("Price of product must be greater than Zero");
+                GetProductDetails(P);
+                DisplayDetails(P);
 
 
-                    Console.WriteLine("Product Details :");
-                    Console.WriteLine("Product ID :" + P.ProductID);
-                    Console.WriteLine("Product Name :" + P.ProductName);
-                    Console.WriteLine("Product Price :" + P.ProductPrice);
-                }
-                catch (DataEntryException ex)
-                {
-                    Console.WriteLine(ex.Message);
+            }
+            catch (DataEntryException ex)
+            {
+                Console.WriteLine(ex.Message);
 
-                }
-                catch (SystemException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+            }
+            catch (SystemException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
-                Console.ReadKey();
+            Console.ReadKey();
 
-            } while (true);
 
-           
+
+
         }
 
     }
